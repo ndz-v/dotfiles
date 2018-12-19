03d4f9233debf15c82bff1c04895db2a6dd49894
nidzo <16163816+nidzov@users.noreply.github.com>
2018-12-19 18:09:38 +0100

    update

--- remote-setup.sh
+++ remote-setup.sh
@@ -4,5 +4,4 @@ echo "Installing dotfiles"
 
 mkdir -p "$HOME/Projects/dotfiles" && \
 eval "wget --no-check-certificate -O - https://github.com/nidzov/dotfiles/tarball/master | tar -xzv -C $HOME/Projects/dotfiles --strip-components=1"
-
-eval "bash $HOME/Projects/dotfiles/setup.sh"
+. "$HOME/Projects/dotfiles/setup.sh"
