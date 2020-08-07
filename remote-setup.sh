404c5874bf024c6d95d4bcd59b9d38051da12892
Nikola Vidic <16163816+ndz-v@users.noreply.github.com>
2020-08-07 23:42:18 +0200

    Update remote-setup.sh

--- remote-setup.sh
+++ remote-setup.sh
@@ -3,5 +3,5 @@
 echo "Installing dotfiles"
 
 mkdir -p "$HOME/dev/dotfiles" &&
-    eval "wget --no-check-certificate -O - https://github.com/nidzov/dotfiles/tarball/master | tar -xzv -C $HOME/dev/dotfiles --strip-components=1"
+    eval "wget --no-check-certificate -O - https://github.com/ndz-v/dotfiles/tarball/master | tar -xzv -C $HOME/dev/dotfiles --strip-components=1"
 eval "bash $HOME/dev/dotfiles/setup.sh 2>&1 | tee setup.log"
