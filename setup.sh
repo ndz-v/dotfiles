e1c6be2d6face716af41c0deb58e74513f4007a2
nidzo <16163816+ndz-v@users.noreply.github.com>
2021-09-15 18:30:55 +0200

    Remove unused packages to install

--- setup.sh
+++ setup.sh
@@ -18,18 +18,13 @@ apps=(
     bat                     # Alternative to cat
     curl                    # Make sure curl is installed
     entr                    # Rebuild project if sources change
-    exfat-fuse              # exFat
-    exfat-utils             # exFat
     fd-find                 # Alternative to find
     ffmpeg                  # Needed for youtube-dl to work
-    fzf                     # General-purpose command-line fuzzy finder
     git                     # Versioncontrol
     graphviz                # Create graphs
     grub-customizer         # Customize grub with GUI
-    kid3                    # Tagging mp3 files
     latexmk                 # Completely automates the process of generating a LaTeX document
     latte-dock              # Dock for kde plasma desktop
-    libnotify-bin           # Desktop notifications
     lm-sensors              # Read sensors
     neovim                  # Text editor
     postgresql              # Database
