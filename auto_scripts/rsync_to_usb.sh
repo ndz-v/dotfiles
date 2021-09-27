6b630beda37a13c59f773e777c22be13d52139d9
nidzo <16163816+ndz-v@users.noreply.github.com>
2021-09-27 19:09:43 +0200

    Specify backup location in rsync scripts

--- auto_scripts/rsync_to_usb.sh
+++ auto_scripts/rsync_to_usb.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-sleep 10
+sleep 5
 if cut -d' ' -f2 /proc/mounts | \grep -q "/media/nidzo/DATA\$"; then
+
+    su - nidzo -c "rsync -rvhau /home/nidzo//Documents/ /media/nidzo/DATA/all_Documents --log-file=/home/nidzo/rsync.log;
+    rsync -rvhau /home/nidzo//LaTeX/ /media/nidzo/DATA/all_LaTeX --log-file=/home/nidzo/rsync.log;
+    rsync -rvhau /home/nidzo//Music/ /media/nidzo/DATA/all_Music --log-file=/home/nidzo/rsync.log;
+    rsync -rvhau /home/nidzo//dev/ /media/nidzo/DATA/all_dev --log-file=/home/nidzo/rsync.log"
+
     su - nidzo -c "rsync -rvhau --exclude='dotfiles' --delete \
     /home/nidzo/.ssh \
     /home/nidzo/.thunderbird \
--- scripts/rsync_functions.sh
+++ scripts/rsync_functions.sh
@@ -5,31 +5,44 @@
 # h human readable numbers
 # a archive mode
 # u skip files that are newer on the reciever
+rsync_args=(-rvhau --exclude node_modules --exclude Debug --exclude publish --exclude bin --exclude out)
 
 rsync_up() {
+    if cut -d' ' -f2 /proc/mounts | \grep -q "/media/nidzo/DATA\$"; then
 
-    rsync -rvhau --exclude='dotfiles' --delete "$HOME/.ssh" \
-        "$HOME/.thunderbird" \
-        "$HOME/Desktop" \
-        "$HOME/Documents" \
-        "$HOME/Downloads" \
-        "$HOME/LaTeX" \
-        "$HOME/Music" \
-        "$HOME/Pictures" \
-        "$HOME/Public" \
-        "$HOME/Videos" \
-        "$HOME/dev" \
-        /media/nidzo/DATA/linux
+        rsync "${rsync_args[@]}" "$HOME/Documents/" /media/nidzo/DATA/all_Documents --log-file="$HOME/rsync.log"
+        rsync "${rsync_args[@]}" "$HOME/LaTeX/" /media/nidzo/DATA/all_LaTeX --log-file="$HOME/rsync.log"
+        rsync "${rsync_args[@]}" "$HOME/Music/" /media/nidzo/DATA/all_Music --log-file="$HOME/rsync.log"
+        rsync "${rsync_args[@]}" "$HOME/dev/" /media/nidzo/DATA/all_dev --log-file="$HOME/rsync.log"
+
+        rsync "${rsync_args[@]}" --exclude='dotfiles' --delete \
+            "$HOME/.ssh" \
+            "$HOME/.thunderbird" \
+            "$HOME/Desktop" \
+            "$HOME/Documents" \
+            "$HOME/Downloads" \
+            "$HOME/LaTeX" \
+            "$HOME/Music" \
+            "$HOME/Pictures" \
+            "$HOME/Public" \
+            "$HOME/Videos" \
+            "$HOME/dev" \
+            /media/nidzo/DATA/linux_backup --log-file="$HOME/rsync.log"
+    fi
 }
 
-rsyunc_down() {
-    rsync -rvhau -P --exclude='dotfiles' \
-        /media/nidzo/DATA/linux/ \
-        "$HOME/"
+rsync_down() {
+    if cut -d' ' -f2 /proc/mounts | \grep -q "/media/nidzo/DATA\$"; then
+        rsync "${rsync_args[@]}" "${rsync_args[@]}" --exclude='dotfiles' \
+            /media/nidzo/DATA/linux_backup/ \
+            "$HOME/"
+    fi
 }
 
 rsync_up_dir() {
-    rsync -rvhau --exclude='dotfiles' \
-        "$HOME/$1" \
-        /media/nidzo/DATA/linux
+    if cut -d' ' -f2 /proc/mounts | \grep -q "/media/nidzo/DATA\$"; then
+        rsync "${rsync_args[@]}" --exclude='dotfiles' \
+            "$HOME/$1" \
+            /media/nidzo/DATA/linux_backup
+    fi
 }
